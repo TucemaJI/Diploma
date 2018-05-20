d947bec9c768409e78198ac926d8fc41597154b8
Inugami <bidi75518@gmail.com>
2018-05-20 14:12:09 +0300

    Finder

---
+++ ConsoleVisual/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Finder
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string path = GetPath();
+            var service = new FinderService();
+
+            var thread = new Thread(new ParameterizedThreadStart(service.Search));
+            thread.Start(path);
+
+            service.Pause();
+        }
+
+        private static string GetPath()
+        {
+            Console.Write("Please enter the directory where is the file (like D:\\ ):");
+            string path = Console.ReadLine();
+            return path;
+        }
+    }
+}
---
+++ FinderService/FinderService.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+
+namespace Finder
+{
+    public class FinderService
+    {
+        static bool pause = true;
+        AutoResetEvent waitHandler = new AutoResetEvent(pause);
+
+        public void Search(object location)
+        {
+            waitHandler.WaitOne();
+            string locationToSearch = location.ToString();
+
+            if (!locationToSearch.EndsWith("\\"))
+            {
+                locationToSearch += '\\';
+            }
+
+            string result = string.Empty;
+            var files = new List<string>();
+            var directories = new List<string>();
+
+            string[] fileEntries = Directory.GetFiles(locationToSearch);
+            foreach (string file in fileEntries)
+            {
+                files.Add(file);
+            }
+            ToConsole(files);
+
+            string[] subdirectoryEntries = Directory.GetDirectories(locationToSearch);
+
+            foreach (string subdirectory in subdirectoryEntries)
+            {
+                ToConsole(subdirectory);
+                var subdirs = Directory.GetAccessControl(subdirectory);
+                if (!subdirs.AreAccessRulesProtected)
+                {
+                    var thread = new Thread(new ParameterizedThreadStart(Search));
+                    thread.Start(subdirectory);
+                }
+            }
+            waitHandler.Set();
+        }
+
+        public void Pause()
+        {
+            var consoleKey = Console.ReadKey();
+            if (consoleKey.Key == ConsoleKey.Spacebar)
+            {
+                if (!pause)
+                {
+                    pause = true;
+                }
+                else { pause = false; }
+            }
+        }
+
+        public void ToConsole(List<string> files)
+        {
+            foreach (var file in files)
+            {
+                Console.WriteLine(file);
+            }
+        }
+
+        public void ToConsole(string foulder)
+        {
+            Console.WriteLine(foulder);
+        }
+    }
+}
